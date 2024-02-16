a6adec4c807b5048f73dcf45dc723b7f179f75ba
Walter Gomar <wgomarg@miumg.edu.gt>
2024-02-16 17:46:30 -0600

    Menu de opciones creado

--- Tarea_1/lista_doblemente_enlazada.py
+++ Tarea_1/lista_doblemente_enlazada.py
@@ -1,6 +1,44 @@
-print("hola mundo")
+def insertar_inicio():
+    dato = input("Ingrese un dato: ")
+    print("se ha insertado el dato: "+dato+" al inicio de la lista")
 
-print("Ingrese un numero")
-numero = int(input(": "))
+def insertar_final():
+    dato = input("Ingrese un dato: ")
+    print("se ha insertado el dato: "+dato+" al final de la lista")
 
-print(f"El numero que ingreso es {numero}")
+def eliminar_por_valor():
+    dato = input("Ingrese un dato: ")
+    print("se ha eliminado el dato: "+dato+" de ña lista")
+
+def mostrar_lista():
+    print("elementos de la lista doblemente enlazada")
+
+def Salir():
+    exit()
+
+def invalido():
+    print("opcion no válida, ingrese un número del 1 al 5")
+
+while True:
+    print("Menu de operaciones: lista doblemente enlazada")
+    print("1. Insertar al inicio de la lista")
+    print("2. Insertar al final de la lista")
+    print("3. Elimnar de la lista un valor específico")
+    print("4. Mostar lista")
+    print("5. Salir del programa")
+    valor = input("Seleccione una opcion: ")
+    if(valor == "1"):
+        insertar_inicio()
+        continuar = input("Presione ENTER para continuar: ")
+    elif(valor == "2"):
+        insertar_final()
+        continuar = input("Presione ENTER para continuar: ")
+    elif(valor == "3"):
+        eliminar_por_valor()
+        continuar = input("Presione ENTER para continuar: ")
+    elif(valor == "4"):
+        mostrar_lista()
+        continuar = input("Presione ENTER para continuar: ")
+    elif(valor == "5"):
+        Salir()
+    else: invalido()
