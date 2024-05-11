ee1c3be86465d7f5ccc845ab28078306078d9676
Daniel Martínez <damartinez@swift-solutions.co>
2024-05-11 11:53:00 -0600

    tarea 4 creada

---
+++ Tarea_4/matriz.py
@@ -0,0 +1,8 @@
+class Matriz:
+    def __init__(self, filas, columnas):
+        self.filas = filas
+        self.columnas = columnas
+        self.data = {}
+
+    def insertar_datos(self):
+        pass
